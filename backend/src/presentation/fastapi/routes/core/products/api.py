fe526a04bd30aeff52494ddd0ad56dce3b2a3acc
LizaG000 <elizaveta.vladimirovna.g@gmail.com>
2025-10-21 02:16:56 +0300

    v

--- backend/src/presentation/fastapi/routes/core/products/api.py
+++ backend/src/presentation/fastapi/routes/core/products/api.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 from fastapi import status
 from dishka import FromDishka
+from dishka.integrations.fastapi import DishkaRoute
 from src.usecase.products.schemas import CreateProductBatchSchema
 from src.usecase.products.create import CreateProductUsecase
 
-ROUTER = APIRouter( tags=["Products"])
+ROUTER = APIRouter(route_class=DishkaRoute, tags=["Products"])
 
 @ROUTER.post('', status_code=status.HTTP_200_OK)
-async def create_products_batch(
+async def create_products(
     usecase: FromDishka[CreateProductUsecase],
     data: list[CreateProductBatchSchema]
 ) -> None:
