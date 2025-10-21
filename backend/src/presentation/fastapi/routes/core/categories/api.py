13d48f2ff9c80f2b461fc02397dfd352c895da8b
LizaG000 <elizaveta.vladimirovna.g@gmail.com>
2025-10-22 01:20:02 +0300

    правлена ручка создания продуктов, правлены ручки созданипя категорий, материалов, цветов, так же написаны ручки для их получения

--- backend/src/presentation/fastapi/routes/core/categories/api.py
+++ backend/src/presentation/fastapi/routes/core/categories/api.py
@@ -1,15 +1,27 @@
 from dishka.integrations.fastapi import DishkaRoute
 from dishka.integrations.fastapi import FromDishka
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter
 from fastapi import status
 from src.application.schemas.common import PatternSchema, CreatePatternSchema
 from src.infra.postgres.tables import CategoriesModel
-from src.infra.postgres.gateways.base import CreateReturningGate
+from src.infra.postgres.gateways.base import CreateReturningGate, GetAllGate
+
 
 ROUTER = APIRouter(route_class=DishkaRoute)
 
 @ROUTER.post('', status_code=status.HTTP_200_OK)
 async def create_category(
+    session: FromDishka[AsyncSession],
     usecase: FromDishka[CreateReturningGate[CategoriesModel, CreatePatternSchema, PatternSchema]],
     category: CreatePatternSchema) -> PatternSchema:
-    return await usecase(category)
+    async with session.begin():
+        return await usecase(category)
+
+
+@ROUTER.get('', status_code=status.HTTP_200_OK)
+async def create_category(
+    usecase: FromDishka[GetAllGate[CategoriesModel, PatternSchema]]
+    ) -> list[PatternSchema]:
+    return await usecase()
+
