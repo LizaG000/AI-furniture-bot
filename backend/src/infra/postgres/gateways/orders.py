2efd4f897549589d5c564b79417330e7b4d9cff0
ejik-sasha <112908553+ejik-sasha@users.noreply.github.com>
2025-10-24 16:50:13 +0300

    к

--- backend/src/infra/postgres/gateways/orders.py
+++ backend/src/infra/postgres/gateways/orders.py
@@ -61,6 +61,6 @@ class GetOrdersGate(PostgresGateway):
 
         if not result:
             logger.info("Нет заказов.")
-            return result == []
+            return []
 
         return [ReturnOrderSchema.model_validate(row) for row in result]
